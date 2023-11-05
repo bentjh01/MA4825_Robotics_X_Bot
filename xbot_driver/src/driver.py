6de967246ac98f5309fbe598985f3116c64f7f5d
bentjh01 <benjamintehjh01@gmail.com>
2023-11-05 17:04:35 +0800

    working

--- xbot_driver/src/driver.py
+++ xbot_driver/src/driver.py
@@ -30,28 +30,26 @@ class XBotDriver():
         self.motor_2 = AX1xA("motor_2", 2, self.portHandler)
         self.motor_3 = AX1xA("motor_3", 3, self.portHandler)
         self.motor_4 = AX1xA("motor_4", 4, self.portHandler)
-        self.motor_5 = AX1xA("motor_5", 5, self.portHandler)
+        # self.motor_5 = AX1xA("motor_5", 5, self.portHandler)
         self.motor_6 = AX1xA("motor_6", 6, self.portHandler)
 
-        self.motors = [self.motor_1, self.motor_2, self.motor_3, self.motor_4, self.motor_5, self.motor_6]
+        self.motors = [self.motor_1, self.motor_2, self.motor_3, self.motor_4, self.motor_6]
 
         for motor in self.motors:
             motor.set_torque_mode(True)
 
         rospy.init_node('xbot_driver')
-        self.state_publisher = rospy.Publisher('motor_states', AX, queue_size=10)
-        self.set_position_subscriber = rospy.Subscriber('set_position', AX, self.set_position_callback)
-        self.set_position_rate = rospy.Rate(10)
+        self.state_publisher = rospy.Publisher('/motor_states', AX, queue_size=10)
+        self.set_position_subscriber = rospy.Subscriber('/set_position', AX, self.set_position_callback)
         self.state_publish_rate = rospy.Rate(10)
 
-    def set_positon_callback(self, msg):
+    def set_position_callback(self, msg):
+        rospy.loginfo(msg.ID)
         for motor in self.motors:
-            if msg.ID == motor.ID:
-                motor.set_position(msg.Goal_Position)
-                if msg.Moving_Speed == 0:
-                    continue
+            if msg.ID == motor.ID and not motor.get_moving():
                 motor.set_moving_speed(msg.Moving_Speed)
-        self.set_position_rate.sleep()
+                motor.set_goal_position(msg.Goal_Position)
+                motor.set_led(msg.LED)
 
     def motor_state_update(self):
         for motor in self.motors:
@@ -69,11 +67,10 @@ class XBotDriver():
         print('Running')
         while not rospy.is_shutdown():
             self.set_position_subscriber
-            self.motor_state_update()
+            # self.motor_state_update()
     
 if __name__ == "__main__":
     xBotDriver = XBotDriver()
-    try:
-        xBotDriver.main()
-    except rospy.ROSInterruptException:
-        pass
+    xBotDriver.main()
+    # except rospy.ROSInterruptException:
+        # pass
