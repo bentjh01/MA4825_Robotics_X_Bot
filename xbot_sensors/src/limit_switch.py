0547cc498fe7370d9cbd18a3caa9f32a7c652135
bentjh01 <benjamintehjh01@gmail.com>
2023-11-07 21:46:37 +0800

    C&P working code

--- xbot_sensors/src/limit_switch.py
+++ xbot_sensors/src/limit_switch.py
@@ -1,1 +1,27 @@
-#TODO-copy and paste code for limit switch readings
+
+import rospy
+from std_msgs.msg import Bool
+import Jetson.GPIO as GPIO
+
+def main():
+   channel = 15
+   GPIO.setmode(GPIO.BOARD)
+   GPIO.setup(channels = channel, direction = GPIO.IN)
+   topic_name = "/limit_switch"
+   limitSwitchPublusher = rospy.Publisher(topic_name, Bool, queue_size=10)
+   rospy.init_node('LimitSwitchWatcherNode')
+   rate = rospy.Rate(10)
+   while not rospy.is_shutdown():
+      status = GPIO.input(channel)
+      msg = Bool()
+      msg.data = bool(status)
+      limitSwitchPublusher.publish(msg)
+      rospy.loginfo(msg)
+      rate.sleep()
+   GPIO.cleanup()
+
+if __name__ == '__main__':
+   try:
+      main()
+   except rospy.ROSInterruptException:
+      pass
