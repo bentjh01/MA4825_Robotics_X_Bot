797ae43a5ace099713b48ed84e18591d562f7ba4
bentjh <benjamintehjh@outlook.com>
2023-11-07 10:02:51 +0800

    TODO-script for hardcode

--- test_scripts/get_joint_positions.py
+++ test_scripts/get_joint_positions.py
@@ -1,2 +1,3 @@
 #TODO-subs to xbot_driver/motor_states to get real-time updates
 #TODO-print the states
+#TODO-Optionally add publisher to xbxbot simple controller to move to the read point to verify
