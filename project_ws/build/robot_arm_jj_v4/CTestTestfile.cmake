64e32e9135a5e8bf7d93c899055e945c9ac953a8
bentjh <benjamintehjh@outlook.com>
2023-11-14 21:28:08 +0800

    QiYuan_ws

--- project_ws/build/robot_arm_jj_v4/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson/project_ws/src/robot_arm_jj_v4
-# Build directory: /home/jetson/project_ws/build/robot_arm_jj_v4
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- xbot-ui/README.md
+++
@@ -1,2 +0,0 @@
-# ros-ui-react
-A collection of React components for interacting with robotic systems running ROS.
