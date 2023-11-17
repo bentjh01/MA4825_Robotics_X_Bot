af37217a4a10e43d45f8e7b829b646b8a232d173
bentjh <benjamintehjh@outlook.com>
2023-11-17 12:14:52 +0800

    final commit

--- xbot_kinematics/src/inverse_kinematics.py
+++ xbot_kinematics/src/inverse_kinematics.py
@@ -7,6 +7,9 @@ class InverseKinamatics():
     def __init__(self) -> None:
         self.__init__rospy()
         self.move_group = MoveGroupComander('robot_arm')
+        self.goal_joint_states=JointState()
+        self.last_time = rospy.get_rostime()
+        self.ready = False
         
     def __init__rospy(self):
         rospy.init_node('xbot_kinematics')
@@ -20,9 +23,16 @@ class InverseKinamatics():
         sucess = self.move_group.go(wait=True)
 
     def set_joint_callback(self, msg):
-        self.set_states_publisher.publish(msg)
+        self.goal_joint_states=msg
+        self.last_time = rospy.get_rostime()
 
     def run(self):
         while not rospy.is_shutdown():
             self.locker_pose_subscriber 
             self.set_joint_subscriber
+            if self.ready:
+                self.set_states_publisher.publish(self.goal_joint_states)
+                self.ready = False
+            if rospy.get_rostime() - self.last_time > 2:
+                self.ready = True
+
